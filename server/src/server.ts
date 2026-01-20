9f568e5f20df8f52d69e9bcf3faa4da247d2ce17
Swapnil Sahare <swapnilsahare45@gmail.com>
2026-01-20 18:50:26 +0530

    update cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -31,6 +31,9 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+    exposedHeaders: ["Set-Cookie"],
   }),
 );
 app.use(cookieParser());
